cd2a17df87b1610de8bead6dd1b8d37a606d88db
Jonas Malsbenden <jm4061s@fh-aachen.de>
2023-02-09 11:48:15 +0100

    Dice class and comments

--- src/main/java/backend/gameBoard/GameBoard.java
+++ src/main/java/backend/gameBoard/GameBoard.java
@@ -1,18 +1,31 @@
 package backend.gameBoard;
 
 import backend.enums.RoomType;
-
+/**
+ * Gameboard class
+ * <p>
+ *     generate a Gameboard where rooms ca be placed in<br>
+ *     sets the size of the game
+ * </p>
+ *
+ * @author jonasmalsbenden
+ */
 public class GameBoard {
     public RoomField[][] board;
-
+    /**
+     * Constructor
+     * (generates a Gamboard with a given size)
+     */
     public GameBoard(int size){
         board = new RoomField[size][size];
     }
-
-
+    /**
+     * Default Constructor
+     * (generates a Gameboard with a size fo 20)
+     */
     public GameBoard(){
         board = new RoomField[20][20];
-        autofill();
+        generateLayout();
         for (int x = 0; x < 20; x++){
             for (int y = 0; y < 20; y++){
                 System.out.print(board[x][y]);
@@ -21,7 +34,11 @@ public class GameBoard {
         }
 
     }
-    public void autofill() {
+    /**
+     * generate Layout
+     * (generates a gameboard layout randomly)
+     */
+    public void generateLayout() {
         //To-Do
         //automaticly generate Gameboard with rooms and hallways
     }
---
+++ src/main/java/backend/logic/Dice.java
@@ -0,0 +1,39 @@
+package backend.logic;
+/**
+ * Dice class
+ * <p>
+ *     generate a Dice and roll it<br>
+ *     Dices have variable sizes
+ * </p>
+ *
+ * @author jonasmalsbenden
+ */
+public class Dice {
+    protected int dSize ;
+    /**
+     * Default constructor
+     * (generates a Dice with size 10)
+     */
+    public Dice(){
+        dSize = 10;
+    }
+    /**
+     * Constructor
+     * (generates a Dice with any size)
+     */
+    public Dice(int DSize){
+        dSize = DSize;
+    }
+    /**
+     * Dice roll
+     * (generates a random number between 0 and the Dice size)
+     */
+    public int roll(){
+        int result = (int) (Math.random()*dSize);
+        return result;
+    }
+
+    public int getDSize() {
+        return dSize;
+    }
+}
