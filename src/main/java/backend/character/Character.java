a54d31a30654960f55d98c7a0ea36d0508a994ff
olea <olea.plum@gmail.com>
2023-02-09 09:20:33 +0100

    added race and ability class attributes

--- src/main/java/backend/character/Character.java
+++ src/main/java/backend/character/Character.java
@@ -1,4 +1,65 @@
 package backend.character;
 
+import backend.enums.Race;
+import backend.enums.Stat;
+
 public class Character {
+    private Race race;
+    private int strength;
+    private int dexterity;
+    private int constitution;
+    private int intelligence;
+    private int wisdom;
+
+    public Race getRace() {
+        return race;
+    }
+
+    public void setRace(Race race) {
+        this.race = race;
+    }
+
+    public int getStat(Stat stat) {
+        int value = 0;
+
+        switch (stat) {
+            case STR -> value = strength;
+            case DEX -> value = dexterity;
+            case CON -> value = constitution;
+            case INT -> value = intelligence;
+            case WIS -> value = wisdom;
+        }
+
+        return value;
+    }
+
+    public void setStat(Stat stat, int value) {
+        switch (stat) {
+            case STR -> setStrength(value);
+            case DEX -> setDexterity(value);
+            case CON -> setConstitution(value);
+            case INT -> setIntelligence(value);
+            case WIS -> setWisdom(value);
+        }
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public void setDexterity(int dexterity) {
+        this.dexterity = dexterity;
+    }
+
+    public void setConstitution(int constitution) {
+        this.constitution = constitution;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public void setWisdom(int wisdom) {
+        this.wisdom = wisdom;
+    }
 }
