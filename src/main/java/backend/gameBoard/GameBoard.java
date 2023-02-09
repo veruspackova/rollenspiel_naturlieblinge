aae420a33077383fe58a524d106e37c230a6770f
Jonas Malsbenden <jm4061s@fh-aachen.de>
2023-02-09 08:13:38 +0100

    Gameboard Baseclass

--- src/main/java/backend/gameBoard/GameBoard.java
+++ src/main/java/backend/gameBoard/GameBoard.java
@@ -1,5 +1,13 @@
 package backend.gameBoard;
 
 public class GameBoard {
-    
+    public RoomField[][] board;
+
+    public GameBoard(int size){
+        board = new RoomField[size][size];
+    }
+
+    public void autofill(){
+
+    }
 }
---
+++ src/main/java/backend/misc/Item.java
@@ -0,0 +1,2 @@
+package backend.misc;public class Item {
+}
