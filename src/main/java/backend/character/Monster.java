2d3a3be15a4e56660e66a232c5e8a4f246d3aecd
Sander Görtz <sander.goertz@gmx.de>
2023-02-16 08:30:53 +0100

    Character Class: Monster
    * adds parameters for hp, armour class and hit dice due to not all monsters having equal of those

--- src/main/java/backend/character/Monster.java
+++ src/main/java/backend/character/Monster.java
@@ -4,14 +4,14 @@ import backend.enums.Race;
 import backend.enums.Stat;
 import backend.logic.Dice;
 
-//@todo werte einpflegen nach antwort im forum
+// HP, Armour Class and Hit Dice are parameters due to not all monsters being equal
 
 public class Monster extends Character{
-    public Monster(Race race, String name, int strength, int dexterity, int constitution, int intelligence, int wisdom) {
+    public Monster(Race race, String name, int strength, int dexterity, int constitution, int intelligence, int wisdom, int hp, int armourClass, int hitDice) {
         super(race, name, strength, dexterity, constitution, intelligence, wisdom);
-        setHitPoints(10 + getStatModifier(Stat.CON));
-        setArmourClass(10);
-        setAttackDice(new Dice(10));
+        setHitPoints(hp + getStatModifier(Stat.CON));
+        setArmourClass(armourClass);
+        setAttackDice(new Dice(hitDice));
         super.setHitDiceAvailable(0);
     }
 }
