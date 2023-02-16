17b450cc5a08db4830408c057b55e05bd7c4fdc3
Sander Görtz <sander.goertz@gmx.de>
2023-02-16 08:33:41 +0100

    Character Class: Monster
    * removes parameter race from monster since they should always have no bonus so their race is always none

--- src/main/java/backend/character/Monster.java
+++ src/main/java/backend/character/Monster.java
@@ -7,8 +7,8 @@ import backend.logic.Dice;
 // HP, Armour Class and Hit Dice are parameters due to not all monsters being equal
 
 public class Monster extends Character{
-    public Monster(Race race, String name, int strength, int dexterity, int constitution, int intelligence, int wisdom, int hp, int armourClass, int hitDice) {
-        super(race, name, strength, dexterity, constitution, intelligence, wisdom);
+    public Monster(String name, int strength, int dexterity, int constitution, int intelligence, int wisdom, int hp, int armourClass, int hitDice) {
+        super(Race.NONE, name, strength, dexterity, constitution, intelligence, wisdom);
         setHitPoints(hp + getStatModifier(Stat.CON));
         setArmourClass(armourClass);
         setAttackDice(new Dice(hitDice));
