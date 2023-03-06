42995aac75d6473e79f2558787b8ccdd3cb94721
Sander Görtz <sander.goertz@gmx.de>
2023-03-06 09:26:46 +0100

    ISearchable implementation
    * Adds import so search uses the right Character class

--- src/main/java/backend/artifacts/ISearchable.java
+++ src/main/java/backend/artifacts/ISearchable.java
@@ -1,6 +1,7 @@
 package backend.artifacts;
 
 import java.util.ArrayList;
+import backend.character.Character;
 
 public interface ISearchable {
     public ArrayList<ISearchable> search(Character character);
