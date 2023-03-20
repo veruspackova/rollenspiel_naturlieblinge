342e860d9120780863c35d94419dcc14777d1072
olea <olea.plum@gmail.com>
2023-03-20 08:36:42 +0100

    add enums for spells

---
+++ src/main/java/backend/enums/Spells.java
@@ -0,0 +1,21 @@
+package backend.enums;
+
+public enum Spells {
+    //burning hands: Attack and damage
+    //fireball: attack and damage (ranged attack)
+    //ray of frost: attack and damage (ranged attack)
+    //mage armor: + d8 AC
+    //shield: + d8 AC all on field
+    //2 x heal: + d8 LP
+    //sleep: no attack d4 rounds
+    //magic mist: -4d on enemy attack (hides / invisible)
+
+    BURNING_HANDS,
+    FIREBALL,
+    RAY_OF_FROST,
+    MAGE_ARMOUR,
+    SHIELD,
+    HEAL,
+    SLEEP,
+    MAGIC_MIST,
+}
