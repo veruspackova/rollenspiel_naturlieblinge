713d4a161571ac95b1f076ceb06f5eb3e74858a4
Alexander.Wolff <alexander.wolff@gmail.com>
2023-03-27 11:17:22 +0200

    Dart implementation and adaptation to use 20 darts

--- src/main/java/backend/artifacts/weapons/ranged/Dart.java
+++ src/main/java/backend/artifacts/weapons/ranged/Dart.java
@@ -2,13 +2,40 @@ package backend.artifacts.weapons.ranged;
 
 import backend.artifacts.weapons.RangedFinesseWeapon;
 import backend.logic.Dice;
-import java.util.ArrayList;
 
     public class Dart extends RangedFinesseWeapon {
     public Dart(){super(new Dice (4),2);}
 
-        ArrayList<String> dart = new ArrayList<String>();
+
+            private int anzahlDart;
+            private int maximaleDart;
+             Dart dart = new Dart(20);
 
 
+            public Dart(int maximaleDart) {
+                this();
+                this.maximaleDart = maximaleDart;
+                this.anzahlDart = maximaleDart;
+            }
 
-}
+            public void dartthrow() {
+                if (anzahlDart > 0) {
+                    anzahlDart--;
+                    System.out.println("A dart was thrown. there remain " + anzahlDart + " darts.");
+                } else {
+                    System.out.println("There are no more darts available.");
+                }
+
+                for (int i = 0; i < 20; i++) {
+                    dart.dartthrow();
+                }
+            }
+
+            public int getAnzahlDart() {
+                return anzahlDart;
+            }
+
+            {
+               System.out.println("there remain " + dart.getAnzahlDart() + " darts.");
+            }
+    }
